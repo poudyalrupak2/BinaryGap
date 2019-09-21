b0a0c6e79b7c80fa780182ea1fe106327c06d4c0
rupak poudyal <rupakpoudyal5@outlook.com>
2019-09-21 19:07:11 +0545

    binarygap solved

--- curcurret/Program.cs
+++ curcurret/Program.cs
@@ -6,7 +6,40 @@ namespace curcurret
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+                Console.WriteLine(p.solution(42));
         }
+        public int solution(int N)
+        {
+            string bits = Convert.ToString(N, 2);
+            //Console.WriteLine($"Bit String: {bits}");
+            int longest = 0;
+            int curCount = 0;
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                if (bits[i] == '0')
+                {
+                    if (curCount > 0) curCount++;
+                    else curCount = 1;
+                }
+                else if (curCount > longest && bits[bits.Length - 1] =='1')
+                {
+                    longest = curCount;
+                    curCount = 0;
+                }
+                else
+                {
+                    if(curCount > longest && bits[i]=='1')
+                        longest = curCount;
+                    curCount = 0;
+                }
+            }
+
+            return longest;
+
+        
     }
+
+}
 }
